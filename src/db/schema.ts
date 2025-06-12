b00d3c57be00258eec42f55560a7065acb279b06
Omer H. <omer.h1910@gmail.com>
2025-06-12 18:44:25 +0300

    feat: enhance database schema and configuration for songs and artists
    - Added new tables for songs, song artists, and artists to the database schema, improving data organization.
    - Updated the existing playlists table to ensure all fields are not nullable.
    - Integrated drizzle-orm for better database management and configuration.
    - Updated .gitignore to include new directories for better project structure.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,27 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const playlistsTable = sqliteTable("playlists", {
   playlistId: text("playlist_id").primaryKey(),
   name: text("name").notNull(),
-  addedAt: integer({ mode: "timestamp" }).default(new Date()),
+  addedAt: integer({ mode: "timestamp" }).notNull(),
+});
+
+export const songsTable = sqliteTable("songs", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  youtubeUrl: text("youtube_url").notNull(),
+  cloudinaryUrl: text("cloudinary_url").notNull(),
+  addedAt: integer({ mode: "timestamp" }).notNull(),
+});
+
+export const songArtistsTable = sqliteTable("song_artists", {
+  songId: text("song_id")
+    .primaryKey()
+    .references(() => songsTable.id),
+  artistId: text("artist_id")
+    .notNull()
+    .references(() => artistsTable.id),
+});
+
+export const artistsTable = sqliteTable("artists", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull().unique(),
 });
